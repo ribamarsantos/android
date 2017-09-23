1a7e18e1afab169d413e27b79a2f6aabc6f59dca
Ribamar Santos <ribamaar@gmail.com>
2017-09-23 11:49:46 -0300

    Aulas Wearables

---
+++ Aula_01_exe02/app/src/main/java/com/ribamarmjs/aula_01_exe02/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ribamarmjs.aula_01_exe02;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
